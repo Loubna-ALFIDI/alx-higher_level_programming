7fe678dc458f922a70d81d4644c5302e8682f963
Loubna <loubna.alfidi@gmail.com>
2023-09-08 00:13:20 +0100

    My first toolbox!

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -49,12 +49,3 @@ def div(a, b):
         The return value. a / b
     """
     return int(a / b)
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
