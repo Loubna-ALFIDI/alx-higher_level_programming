ba5881b66d33db992ef11491efbde684557ff391
Loubna <loubna.alfidi@gmail.com>
2023-09-08 02:54:03 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for str in range(dir(hidden_4)):
-        if dir(hidden_4)[str][:2] == "__":
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][:2] == "__":
             continue
         else:
-            print("{}".format(str))
+            print(dir(hidden_4)[i])
