8c4f1394eebcf21fc979dbb215c7d540d8fc34ee
Loubna <loubna.alfidi@gmail.com>
2023-09-10 20:09:27 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return None
-    length = len(sentence)
-    first = sentence[0]
-    return (length, first)
+        return (0, None)
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        return (length, first)
