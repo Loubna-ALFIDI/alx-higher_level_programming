0c5b01f0c7012af73f0b4475d174695cac7f5e36
Loubna <loubna.alfidi@gmail.com>
2023-09-14 00:35:43 +0100

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = set_1.intersection(set_2)
+    common = set()
+    for element in set_1 & set_2:
+        common.add(element)
     return common
