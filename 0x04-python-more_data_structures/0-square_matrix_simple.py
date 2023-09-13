4cc9831c0b1cd6ac2d5180b5eaf4d12e41cffb98
Loubna <loubna.alfidi@gmail.com>
2023-09-13 23:04:38 +0100

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     square = [list(row) for row in matrix]
-    for i in range(len(square)):
+    for i in square:
         for j in range(len(square)):
-            square[i][j] = square[i][j] ** 2
+            i[j] = i[j] ** 2
     return square
