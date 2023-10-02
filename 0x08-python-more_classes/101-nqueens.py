05be382bcecdf94e8fa1acd30280a2ff9bdc4bf6
Loubna <loubna.alfidi@gmail.com>
2023-10-02 23:24:01 +0100

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 import sys
 
+
 def solve_n_queens(N):
-    def is_safe_to_place(pos, positions_on_chessboard):
-        for i in range(len(positions_on_chessboard)):
-            if positions_on_chessboard[i] == pos or \
-                    positions_on_chessboard[i] - i == pos - len(positions_on_chessboard) or \
-                    positions_on_chessboard[i] + i == pos + len(positions_on_chessboard):
+    def is_safe_to_place(pos, position_oc):
+        for i in range(len(position_oc)):
+            if position_oc[i] == pos or \
+                    position_oc[i] - i == pos - len(position_oc) or \
+                    position_oc[i] + i == pos + len(position_oc):
                 return False
         return True
 
-    def place_queens_on_board(positions_on_chessboard, current_row, N):
+    def place_queens_ob(position_oc, current_row, N):
         if current_row == N:
-            solutions.append(positions_on_chessboard.copy())
+            solutions.append(position_oc.copy())
             return
         for column in range(N):
-            if is_safe_to_place(column, positions_on_chessboard):
-                place_queens_on_board(positions_on_chessboard + [column], current_row + 1, N)
+            if is_safe_to_place(column, position_oc):
+                place_queens_ob(position_oc + [column], current_row + 1, N)
 
     solutions = []
-    place_queens_on_board([], 0, N)
+    place_queens_ob([], 0, N)
     return solutions
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
