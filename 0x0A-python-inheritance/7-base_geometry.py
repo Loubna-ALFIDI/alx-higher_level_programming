13ee8cac6a38cb3487c266b74b6a714d7c2c8fdb
Loubna <loubna.alfidi@gmail.com>
2023-10-10 02:25:41 +0100

    Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validator"""
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
