2095e79d2333c923cfc50dd5e7e4b9587d9c3558
Loubna <loubna.alfidi@gmail.com>
2023-10-10 23:38:54 +0100

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" read_file module """
+
+
+def read_file(filename=""):
+    """ reads a text file """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
