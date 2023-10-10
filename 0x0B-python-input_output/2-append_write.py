58b154ff21836a21438dbc71a7955b4dc76f0a55
Loubna <loubna.alfidi@gmail.com>
2023-10-10 23:46:34 +0100

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" append_write module """
+
+
+def append_write(filename="", text=""):
+    """ writes a text file """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
