4443cf301c89b740cb17060ce5bc1111f0287c35
Loubna <loubna.alfidi@gmail.com>
2023-10-10 23:43:32 +0100

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" write_file module """
+
+
+def write_file(filename="", text=""):
+    """ writes a text file """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
