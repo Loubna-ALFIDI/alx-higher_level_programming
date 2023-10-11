0c4d9bcd9256adcf80efb8858a3ff65a96ddf231
Loubna <loubna.alfidi@gmail.com>
2023-10-11 01:53:09 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" add_item module """
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        list = load_from_json_file("add_item.json")
+    except Exception:
+        list = []
+    list.extend(sys.argv[1:])
+    save_to_json_file(list, "add_item.json")
