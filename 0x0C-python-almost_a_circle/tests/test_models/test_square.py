fff93eda9468a66f43f51a336089fb72a09b7d9d
Loubna <loubna.alfidi@gmail.com>
2023-10-17 00:46:59 +0100

    unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -115,11 +115,11 @@ class testSquare(unittest.TestCase):
     def test_to_dict(self):
         """test to dict"""
         s1 = Square(10, 2, 1)
-        self.assertEqual(str(s1), "[Square] (1) 2/1 - 10")
-        self.assertEqual(s1.to_dictionary(), {'id': 1, 'x': 2, 'size': 10, 'y': 1})
+        self.assertEqual(str(s1), "[Square] (2) 2/1 - 10")
+        self.assertEqual(s1.to_dictionary(), {'id': 2, 'x': 2, 'size': 10, 'y': 1})
         s2 = Square(1, 1)
-        self.assertEqual(str(s2), "[Square] (1) 1/1 - 1")
-        self.assertEqual(s2.to_dictionary(), {'id': 1, 'size': 1, 'x': 1, 'y': 1})
+        self.assertEqual(str(s2), "[Square] (3) 1/0 - 1")
+        self.assertEqual(s2.to_dictionary(), {'id': 3, 'size': 1, 'x': 1, 'y': 0})
 
 if __name__ == '__main__':
     unittest.main()
