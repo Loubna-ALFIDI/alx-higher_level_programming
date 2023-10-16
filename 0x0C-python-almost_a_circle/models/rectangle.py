7e9cd73761de2f5e1f41e3f89ea48bfcafe5995b
Loubna <loubna.alfidi@gmail.com>
2023-10-16 19:13:10 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,7 +86,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """str"""
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}" \
+        return "[Rectangle] ({}) {}/{} - {}/{}" \
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
