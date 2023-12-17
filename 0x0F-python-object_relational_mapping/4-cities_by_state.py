2db63617bec4c21dddda1c28d8a13fdb33a17b57
Loubna <loubna.alfidi@gmail.com>
2023-12-17 01:41:04 +0100

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''Select cities'''
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
