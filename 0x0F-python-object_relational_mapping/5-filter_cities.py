0585e7361b07dc09e3b4b41f477d72c9f440e6ce
Loubna <loubna.alfidi@gmail.com>
2023-12-17 15:51:35 +0100

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+'''Safe filter states'''
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db_user = sys.argv[1]
+    db_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    state_searched = sys.argv[4]
+    safe_inj = (state_searched,)
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=db_user,
+                         passwd=db_pass, db=db_name)
+
+    cur = db.cursor()
+    cur.execute("SELECT name FROM cities WHERE state_id = \
+    (SELECT id FROM states WHERE name = %s LIMIT 1)\
+          ORDER BY id ASC".format(state_searched), safe_inj)
+
+    rows = cur.fetchall()
+
+    tupl = []
+    for row in rows:
+        tupl += row
+    print(", ".join(tupl))
+
+    cur.close()
+    db.close()
