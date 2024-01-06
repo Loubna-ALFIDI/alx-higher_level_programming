9b670b34737491047ee1f21c7a1920127052b078
Loubna <loubna.alfidi@gmail.com>
2024-01-06 21:54:48 +0100

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Error code"""
+from sys import argv
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try: urllib.request.urlopen(req)
+    except urllib.error.URLError as e:
+        print(e.reason)      
+
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
