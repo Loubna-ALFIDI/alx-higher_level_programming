39965d9ace64092779d28cd118dd131c2ddef9e1
Loubna <loubna.alfidi@gmail.com>
2024-01-06 21:26:01 +0100

    Post an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,10 @@ import urllib.request, urllib.parse
 
 if __name__ == "__main__":
     """Main function"""
+    if len(argv) != 3:
+        print("Usage: ./script.py <URL> <email>")
+        exit(1)
+
     email = {'email': argv[2]}
     data = urllib.parse.urlencode(email)
     req = urllib.request.Request(argv[1], data)
