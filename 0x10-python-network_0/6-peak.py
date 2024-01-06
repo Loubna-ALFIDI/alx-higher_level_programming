63ee657fe7cab5ad71b91847eb25052af7a7a368
Loubna <loubna.alfidi@gmail.com>
2024-01-06 02:02:32 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Peak """
+
+def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
+    return find_peak(list_of_integers[1:-1])
