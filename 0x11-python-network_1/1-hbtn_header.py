c07cb9bfe1306be7b64d230dfed235660c958368
Loubna <loubna.alfidi@gmail.com>
2024-01-06 19:53:48 +0100

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Header """
+from sys import argv
+import urllib.request
+
+
+if __name__ == "__main__":
+    """Main function"""
+    with urllib.request.urlopen(argv[1]) as r:
+        print(r.headers.get('X-Request-Id'))
