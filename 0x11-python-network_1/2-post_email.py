b0717f7d92012d048afb76fe46d1322a180b63ff
Loubna <loubna.alfidi@gmail.com>
2024-01-06 21:36:26 +0100

    Post an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         exit(1)
 
     email = {'email': argv[2]}
-    data = urllib.parse.urlencode(email)
+    data = urllib.parse.urlencode(email).encode('utf-8')
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(argv[1]) as res:
         print(res.read().decode('utf-8'))
