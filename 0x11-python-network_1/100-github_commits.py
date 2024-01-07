14b4b28a97280111166ea403e4e398c9ddb253ea
Loubna <loubna.alfidi@gmail.com>
2024-01-07 19:15:19 +0100

    Github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,8 @@ import requests
 
 if __name__ == "__main__":
     """Main function"""
-    url = 'https://api.github.com/repos/{}/{}/commits?per_page={}'.format(
+    url = 'https://api.github.com/repos\
+    /{}/{}/commits?per_page={}'.format(
         argv[2], argv[1], 10
     )
     r = requests.get(url)
