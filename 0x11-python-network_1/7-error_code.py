fff74e63db95ba14c51a5c8aaa843c8a7c996e6f
Loubna <loubna.alfidi@gmail.com>
2024-01-07 18:34:08 +0100

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Error code"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = argv[1]
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        print(res.text)
+    except requests.HTTPError as e:
+        print(f"Error code: {e.response.status_code}")
