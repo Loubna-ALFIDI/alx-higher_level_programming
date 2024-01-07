034291c71c6a3a64737e859850dea7a540bc9fc6
Loubna <loubna.alfidi@gmail.com>
2024-01-07 18:12:21 +0100

    What is my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """ Status """
-import urllib.request
-
+import requests
 
 if __name__ == "__main__":
     """ Main Function """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        html = res.read()
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+    
+    if res.status_code == 200:
+        html = res.content
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    else:
+        print(f"Error: {res.status_code}")
