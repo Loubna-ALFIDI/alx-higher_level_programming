cb0ef46f1ebcd9846b63d999b0e2ceaa3ab73139
Loubna <loubna.alfidi@gmail.com>
2024-01-07 19:12:26 +0100

    Github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Github commits"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[2], argv[1]
+    )
+    r = requests.get(url)
+    commits = r.json()
+    for c in commits:
+        print("{}: {}".format(c.get('sha'), c.get('commit').get('author').get('name')))
