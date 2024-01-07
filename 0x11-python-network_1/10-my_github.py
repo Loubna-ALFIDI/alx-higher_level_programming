031ed5915b8dfdcbce5693ec5fbb64ec3df52a9f
Loubna <loubna.alfidi@gmail.com>
2024-01-07 19:04:28 +0100

    My Github!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""My Github"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
