ef5049e49431afdf0635bb336988c5f2976eabc6
Loubna <loubna.alfidi@gmail.com>
2024-01-07 18:27:24 +0100

    Post an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Email"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """Main function"""
+    url = argv[1]
+    email = {'email': argv[2]}
+    res = requests.post(url, data=email)
+    print(res.text)
