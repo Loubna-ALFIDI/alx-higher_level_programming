f41af775d4e83c02d42ae3685fe397a844134327
Loubna <loubna.alfidi@gmail.com>
2024-01-31 00:44:30 +0100

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const urlAPI = process.argv[2];
+
+request(urlAPI, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const number = {};
+    for (const task of body) {
+      if (task.completed) {
+        number[task.userId] = (number[task.userId] || 0) + 1;
+      }
+    }
+    console.log(number);
+  }
+});
